9598116d5c044b8c3d32311d5e23b6cfe7203d88
kahyunKo <bufar1014@gmail.com>
2024-08-10 08:06:54 +0900

    폴더 추가

--- Program.cs
+++ Program.cs
@@ -16,7 +16,7 @@ namespace kahyun_WinFormsApp1
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            /*Application.Run(new Form1());*/
+            Application.Run(new Form1());
             Application.Run(new Form2());
         }
     }
