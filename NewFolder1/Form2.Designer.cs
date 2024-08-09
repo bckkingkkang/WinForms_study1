8bc2e9e629f48ed8169eb3d225637b720e9dbb01
kahyunKo <bufar1014@gmail.com>
2024-08-10 08:40:03 +0900

    숫자 맞추기 게임 추가

--- NewFolder1/Form2.Designer.cs
+++ NewFolder1/Form2.Designer.cs
@@ -28,12 +28,84 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.display = new System.Windows.Forms.Label();
+            this.inputNum = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.inputBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // display
+            // 
+            this.display.AutoSize = true;
+            this.display.Dock = System.Windows.Forms.DockStyle.Top;
+            this.display.Font = new System.Drawing.Font("굴림", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.display.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.display.Location = new System.Drawing.Point(0, 0);
+            this.display.Margin = new System.Windows.Forms.Padding(0);
+            this.display.Name = "display";
+            this.display.Padding = new System.Windows.Forms.Padding(50);
+            this.display.Size = new System.Drawing.Size(328, 124);
+            this.display.TabIndex = 0;
+            this.display.Text = "게임을 시작합니다.";
+            this.display.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // inputNum
+            // 
+            this.inputNum.Font = new System.Drawing.Font("굴림", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.inputNum.Location = new System.Drawing.Point(12, 200);
+            this.inputNum.Name = "inputNum";
+            this.inputNum.Size = new System.Drawing.Size(123, 63);
+            this.inputNum.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.AutoSize = true;
+            this.button1.BackColor = System.Drawing.Color.White;
+            this.button1.Font = new System.Drawing.Font("굴림", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.button1.ForeColor = System.Drawing.Color.Red;
+            this.button1.Location = new System.Drawing.Point(12, 98);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(288, 81);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "START";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.startBtn);
+            // 
+            // inputBtn
+            // 
+            this.inputBtn.BackColor = System.Drawing.Color.White;
+            this.inputBtn.Font = new System.Drawing.Font("굴림", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.inputBtn.Location = new System.Drawing.Point(148, 200);
+            this.inputBtn.Name = "inputBtn";
+            this.inputBtn.Size = new System.Drawing.Size(151, 63);
+            this.inputBtn.TabIndex = 3;
+            this.inputBtn.Text = "입력";
+            this.inputBtn.UseVisualStyleBackColor = false;
+            this.inputBtn.Click += new System.EventHandler(this.inputBtn_Click);
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.AutoSize = true;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(653, 463);
+            this.Controls.Add(this.inputBtn);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.inputNum);
+            this.Controls.Add(this.display);
+            this.Name = "Form2";
             this.Text = "Form2";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label display;
+        private System.Windows.Forms.TextBox inputNum;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button inputBtn;
     }
 }
--- NewFolder1/Form2.cs
+++ NewFolder1/Form2.cs
@@ -1,20 +1,51 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 using System.Windows.Forms;
 
 namespace kahyun_WinFormsApp1
 {
     public partial class Form2 : Form
     {
+        // 정답
+        private int answer = 0;
+        // 기회
+        private int chance = 0;
+
         public Form2()
         {
             InitializeComponent();
+        }
+
+        private void startBtn(object sender, EventArgs e)
+        {
+            // 시작 버튼 클릭 시 랜덤 변수 생성
+            var rand = new Random();
+            // 1 이상 6 미만
+            answer = rand.Next(1, 6);
+            chance = 10;
+            display.Text = "정답을 입력하세요";
+        }
+
+        // 입력 버튼 클릭 시
+        private void inputBtn_Click(object sender, EventArgs e)
+        {
+            int num = Int32.Parse(inputNum.Text);
+
+            if (num == answer)
+            {
+                display.Text = "WIN";
+            } else
+            {
+                // 기회 1회 감소
+                chance--;
+                display.Text = "오답입니다. 기회는 " + chance + "번 남았습니다.";
+            }
+
+            if(chance <= 0)
+            {
+                display.Text = "LOSE";
+            }
+
         }
     }
 }
